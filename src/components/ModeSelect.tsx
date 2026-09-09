588301e88ac5f0ab4a0b1e49c3563ec69212c56a
duckahiruz <duck-ahiru-Z@users.noreply.github.com>
2026-09-09 11:28:52 +0900

    Add basic mode difficulty artwork

--- src/components/ModeSelect.tsx
+++ src/components/ModeSelect.tsx
@@ -34,6 +34,12 @@ const ModeSelect: React.FC<ModeSelectProps> = ({
     HARD: { titleEn: 'ADVANCED', titleJa: '[上級者](じょうきゅうしゃ)' }
   };
 
+  const normalDifficultyImages: Record<Difficulty, string> = {
+    EASY: '/images/difficulty/beginner.jpg',
+    NORMAL: '/images/difficulty/intermediate.jpg',
+    HARD: '/images/difficulty/advanced.jpg'
+  };
+
   const handleSelectNormal = (diff: Difficulty) => {
     setDifficulty(diff);
     setSelectedModeId('normal');
@@ -61,7 +67,7 @@ const ModeSelect: React.FC<ModeSelectProps> = ({
 
   const selectedData = selectedModeId ? (gameModes as GameModeData[]).find(m => m.id === selectedModeId) : null;
   const selectedDisplayData = selectedData && selectedModeId === 'normal'
-    ? { ...selectedData, ...normalDifficultyTitles[difficulty] }
+    ? { ...selectedData, ...normalDifficultyTitles[difficulty], imageUri: normalDifficultyImages[difficulty] }
     : selectedData;
 
   useEffect(() => {
